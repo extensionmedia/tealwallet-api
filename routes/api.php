10dc04dade364234db8dee43e582125083ca28b2
yassine_desktop <elmeftouhi@extensionmedia.ma>
2022-01-05 21:25:47 +0000

    some route edit

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function(){
+
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
     Route::get('test', [TestController::class,'show']);
 });
 
