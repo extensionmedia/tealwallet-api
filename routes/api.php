a28a8f791eb47ae1ebc31dbca1f4d4439e79e41e
yassine_desktop <elmeftouhi@extensionmedia.ma>
2022-01-07 18:54:43 +0000

    user staff

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\V1\TestController;
+use App\Http\Controllers\V1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,23 +20,32 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
+/****************
+ *
+ *  PUBLIC ROUTES FOR API V1
+ *
+ */
+
 
 Route::prefix('v1')->group(function(){
-
-    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-        return $request->user();
+    Route::get('test', function(){
+        return 'test v1';
     });
-
-    Route::get('test', [TestController::class,'show']);
+    Route::post('/user/register', [UserController::class, 'register']);
+    Route::post('/user/login', [UserController::class, 'login']);
 });
 
+Route::group([
+    'prefix'        =>  'v1',
+    'middleware'    => ['auth:sanctum']
+],
+function(){
+        Route::get('/user/list', [UserController::class, 'index']);
+        Route::post('/user/logout', [UserController::class, 'logout']);
 
 
+        Route::get('test2', function(){
+            return 'test v1';
+        });
 
-
-
-Route::prefix('v2')->group(function(){
-    Route::get('test', function(){
-        return 'test v2';
-    });
 });
